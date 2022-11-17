7cde7f7b31ffdf606339769413869802a483c307
Anantha Madhava <ananthamadhava95@gmail.com>
2022-11-17 12:12:28 +0530

    updated

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,6 +17,20 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/set';
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
@@ -45,7 +59,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
+import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
