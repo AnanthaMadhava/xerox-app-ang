5ab12a1b2303b8431abd9e087a30ebcd72d83f78
Anantha Madhava <ananthamadhava95@gmail.com>
2022-11-17 13:36:15 +0530

    updated

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -117,3 +117,9 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 import 'intl'; // Run `npm install --save intl`.
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+(window as any)['global'] = window;
