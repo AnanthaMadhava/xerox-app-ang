8e59aa027e53866b1bfcf712d98f3a64581fd33f
Anantha Madhava <ananthamadhava95@gmail.com>
2022-11-17 12:45:00 +0530

    updated

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -8,18 +8,15 @@
  *      file.
  *
  * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselves. This includes recent versions of Safari, Chrome (including
- * Opera), Edge on the desktop, and iOS and Chrome on mobile.
+ * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
+ * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
  *
- * Learn more in https://angular.io/guide/browser-support
+ * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
 
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-
-// import promise from 'es6-promise';
-import 'isomorphic-fetch';
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
 import 'core-js/es/symbol';
@@ -36,99 +33,26 @@ import 'core-js/es/regexp';
 import 'core-js/es/map';
 import 'core-js/es/set';
 
-// polyfill all `core-js` features, including early-stage proposals:
-import "core-js";
-// or:
-import "core-js/full";
-// polyfill all actual features - stable ES, web standards and stage 3 ES proposals:
-import "core-js/actual";
-import 'core-js/actual/array/from';
-import 'core-js/actual/array/group';
-import 'core-js/actual/set';        
-import 'core-js/actual/promise';    
-import 'core-js/actual/structured-clone';
-import 'core-js/actual/queue-microtask';
-// polyfill only stable features - ES and web standards:
-import "core-js/stable";
-// polyfill only stable ES features:
-import "core-js/es";
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+import 'classlist.js'; // Run `npm install --save classlist.js`.
 
-// if you want to polyfill `Set`:
-// all `Set`-related features, with early-stage ES proposals:
-import "core-js/full/set";
-// (DOM collections iterator in this case):
-import "core-js/stable/set";
-// only stable ES features required for `Set`:
-import "core-js/es/set";
+/** IE10 and IE11 requires the following to support `@angular/animation`. */
+import 'web-animations-js'; // Run `npm install --save web-animations-js`.
 
-// if you want to polyfill just required methods:
-import "core-js/full/set/intersection";
-import "core-js/actual/array/find-last";
-import "core-js/stable/queue-microtask";
-import "core-js/es/array/from";
-
-// polyfill iterator helpers proposal:
-import "core-js/proposals/iterator-helpers";
-// polyfill all stage 2+ proposals:
-import "core-js/stage/2";
-
-import 'core-js/es';
-import 'core-js/proposals/set-methods';
-import 'core-js/full/set/map';
-
-import "core-js/modules/es.array.unscopables.flat";
-import "core-js/modules/es.array.unscopables.flat-map";
-import "core-js/modules/es.object.from-entries";
-import "core-js/modules/esnext.set.difference";
-import "core-js/modules/esnext.set.intersection";
-import "core-js/modules/esnext.set.is-disjoint-from";
-import "core-js/modules/esnext.set.is-subset-of";
-import "core-js/modules/esnext.set.is-superset-of";
-import "core-js/modules/esnext.set.map";
-import "core-js/modules/esnext.set.symmetric-difference";
-import "core-js/modules/esnext.set.union";
-
-import 'core-js/modules/es.array.iterator';
-import 'core-js/modules/es.object.to-string';
-import 'core-js/modules/es.set';
-import 'core-js/modules/es.array.of';
-
-import 'regenerator-runtime/runtime';
-
-import 'promise-polyfill/src/polyfill';
-
-// promise.polyfill();
-
-/**
- * By default, zone.js will patch all possible macroTask and DomEvents
- * user can disable parts of macroTask/DomEvents patch by setting following flags
- * because those flags need to be set before `zone.js` being loaded, and webpack
- * will put import in the top of bundle, so user need to create a separate file
- * in this directory (for example: zone-flags.ts), and put the following flags
- * into that file, and then add the following code before importing zone.js.
- * import './zone-flags';
- *
- * The flags allowed in zone-flags.ts are listed here.
- *
- * The following flags will work for all browsers.
- *
- * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
- * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
- *
- *  in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
- *  with the following flag, it will bypass `zone.js` patch for IE/Edge
- *
- *  (window as any).__Zone_enable_cross_context_check = true;
- *
- */
+/** Evergreen browsers require these. **/
+import 'core-js/es/reflect';
 
 /***************************************************************************************************
- * Zone JS is required by default for Angular itself.
+ * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/**
+ * Date, currency, decimal and percent pipes.
+ * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
+ */
+import 'intl'; // Run `npm install --save intl`.
