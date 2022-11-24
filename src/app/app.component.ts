e3ed53cffb6a4a58339407f45b15f6bb3058e27b
Anantha Madhava <ananthamadhava95@gmail.com>
2022-11-24 15:27:02 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'xerox-app-ang';
+export class AppComponent implements OnInit {
+  title = 'Xerox App'
+  ngOnInit(){
+    console.log(this.title);
+  }
 }
+
